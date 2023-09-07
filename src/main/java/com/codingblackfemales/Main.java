8e07575dd18dc4bc6d9f14581868ae2f4002df56
Esther Makinde <esthermakinde@hotmail.com>
2023-09-07 21:04:32 +0100

    Cleaned up code. Added do while loop and decimal formatting.

--- src/main/java/com/codingblackfemales/Main.java
+++ src/main/java/com/codingblackfemales/Main.java
@@ -1,5 +1,6 @@
 package com.codingblackfemales;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,49 +14,42 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DecimalFormat df = new DecimalFormat("0.00");
+
         String sourceCurrencyCode = requestUserCurrencyCode(true);
         String destinationCurrencyCode = requestUserCurrencyCode(false);
 
         System.out.println("You have chosen to convert from " + sourceCurrencyCode + " to " + destinationCurrencyCode + ".");
 
-        System.out.println("Provide the amount (in "+ sourceCurrencyCode + ") to be converted to " + destinationCurrencyCode);
+        System.out.println("Provide the amount ,in "+ sourceCurrencyCode + ", to be converted to " + destinationCurrencyCode + ".");
         double amount = Double.parseDouble(scanner.nextLine());
 
-        System.out.println(basicCurrencyConverter.convertCurrency(sourceCurrencyCode, destinationCurrencyCode, amount));
+        System.out.println(df.format(basicCurrencyConverter.convertCurrency(sourceCurrencyCode, destinationCurrencyCode, amount)) + destinationCurrencyCode);
     }
 
 
 
-    public static boolean checkValueInArray(String value, String[] array) {
-        boolean valueInArray = Arrays.asList(array).contains(value);
-        return valueInArray;
+    public static boolean isValueInArray(String value, String[] array) {
+        boolean isvalueInArray = Arrays.asList(array).contains(value);
+        return isvalueInArray;
     }
 
 
 
     // Request and validate user input for currency codes.
-    public static String requestUserCurrencyCode(boolean sourceCode) {
-
+    public static String requestUserCurrencyCode(boolean isSourceCurrencyCode) {
+        String currencyCode;
 
         // Select appropriate message for source code and destination code input prompt.
-        String message1 = (sourceCode == true) ? "What currency do you have? " : "What currency are you converting to? ";
-
-        
-        System.out.println(message1 + "Press the \"Enter\" key for a list of available currency codes.");
-        String currencyCode = scanner.nextLine();
-        currencyCode = currencyCode.toUpperCase();
-
+        String message1 = (isSourceCurrencyCode == true) ? "What currency do you have? " : "What currency are you converting to? ";
 
         // Validate the users' input. If unacceptable, print the list of available codes and prompt the user to update their choice until they choose correctly.
-        while (currencyCode == "" || checkValueInArray(currencyCode, currencyCodesArray) == false) {
-            System.out.println("Available currencies are " + Arrays.toString(currencyCodesArray));
-            System.out.println("Please try again. " + message1);
-            currencyCode = scanner.nextLine();
-            currencyCode = currencyCode.toUpperCase();
-        } 
+        do {
+            System.out.println(message1 + "Available currencies are " + Arrays.toString(currencyCodesArray));
+            currencyCode = scanner.nextLine().toUpperCase();
+        } while (currencyCode == "" || !isValueInArray(currencyCode, currencyCodesArray));
 
-
-        System.out.println("You have selected " + currencyCode);
+        System.out.println("You have selected " + currencyCode + ".");
         
         return currencyCode;
 
